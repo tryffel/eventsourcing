19706cd517b19087e186509234c0ee2293a7ced3
Morgan Hallgren <morgan.hallgren@gmail.com>
2024-04-10 21:20:15 +0200

    try to add testcontainer to easier test esdb event store

--- eventstore/esdb/esdb_test.go
+++ eventstore/esdb/esdb_test.go
@@ -6,9 +6,13 @@
 package esdb_test
 
 import (
+	"context"
+	"fmt"
 	"testing"
 
 	"github.com/EventStore/EventStore-Client-Go/v3/esdb"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 
 	"github.com/hallgren/eventsourcing/core"
 	"github.com/hallgren/eventsourcing/core/testsuite"
@@ -16,9 +20,38 @@ import (
 )
 
 func TestSuite(t *testing.T) {
+	ctx := context.Background()
+
+	req := testcontainers.ContainerRequest{
+		Image:        "eventstore/eventstore:latest",
+		ExposedPorts: []string{"2113/tcp", "1113/tcp"},
+		WaitingFor:   wait.ForListeningPort("2113/tcp"),
+		Cmd:          []string{"--insecure", "--run-projections=All", "--enable-atom-pub-over-http", "--mem-db"},
+	}
+
+	eventstoreContainer, err := testcontainers.GenericContainer(
+		ctx,
+		testcontainers.GenericContainerRequest{
+			ContainerRequest: req,
+			Started:          true,
+		},
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer eventstoreContainer.Terminate(ctx)
+
+	endpoint, err := eventstoreContainer.Endpoint(ctx, "esdb")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println(endpoint)
+
 	f := func() (core.EventStore, func(), error) {
 		// region createClient
-		settings, err := esdb.ParseConnectionString("esdb://localhost:2113?tls=false")
+		settings, err := esdb.ParseConnectionString(endpoint + "?tls=false")
 		if err != nil {
 			return nil, nil, err
 		}
